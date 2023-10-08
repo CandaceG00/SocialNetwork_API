0ee643e03e8116f253d900cdadd1c4c324ec89fd
Candace Gostinski <candacegostinski@Candaces-MBP.lan>
2023-10-08 18:19:29 -0400

    connected users and thoughts to index.js file for models

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Require Users and Thoughts Models
+const Thoughts = require('./models/Thoughts');
+const Users = require('./models/Users');
+
+// Export Modules: Users and Thoughts
+module.exports = { Users, Thoughts };
